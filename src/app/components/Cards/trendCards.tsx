0a9eed01dd4786f4d2db9672f81c05d5abfbce83
matheuspaiva1 <matheuspaiva0007@gmail.com>
2024-09-04 18:20:57 -0300

    feat: create layout main

--- src/app/components/Cards/trendCards.tsx
+++ src/app/components/Cards/trendCards.tsx
@@ -1,7 +1,33 @@
+import Image from 'next/image'
 import React from 'react'
 
 export default function TrendCards() {
   return (
-    <div>TrendCards</div>
+    <div>
+      <h1 className="">Em Alta</h1>
+      <div className="">
+        <div className="">
+          <Image src="" alt="assasin" width={300} height={300} className=''/>      
+          <h1 className="">League of Legends</h1>
+          <p>867.433 jogadores</p>
+          <p>Gratuito</p>
+        </div>
+
+        <div className="">
+          <Image src="" alt="assasin" width={300} height={300} className=''/>      
+          <h1 className="">Dota</h1>
+          <p>777.488 jogadores</p>
+          <p>Gratuito</p>
+        </div>
+
+        <div className="">
+          <Image src="" alt="assasin" width={300} height={300} className=''/>      
+          <h1 className="">Resident Evil 0</h1>
+          <p>999.125 jogadores</p>
+          <p>R$ 129,90</p>
+        </div>
+
+      </div>
+    </div>
   )
 }
--- src/app/components/MainBg/bgMain.tsx
+++ src/app/components/MainBg/bgMain.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import React from 'react'
 import TitleCard from '../Cards/titleCard'
+import TrendCards from '../Cards/trendCards'
 
 
 export default function BgMain() {
   return (
     <div>
       <TitleCard/>
+      <TrendCards/>
       
     </div>
   )
